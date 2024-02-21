dce8474a9938324aaffd4f11b178da71cda57302
Balmy Gazebo <balmy.gazebo_0f@icloud.com>
2024-02-21 14:04:39 -0500

    Remove Group Account

--- programs/wen-royalty-distribution/src/state.rs
+++ programs/wen-royalty-distribution/src/state.rs
@@ -18,28 +18,3 @@ impl DistributionAccount {
 }
 
 pub const ROYALTY_BASIS_POINTS_FIELD: &str = "royalty_basis_points";
-
-/// Temporary accounts while waiting for Token2022 mainnet on Group and Member Accounts
-
-/// Data struct for a `TokenGroupMember`
-#[account()]
-pub struct TokenGroupMember {
-    /// The associated mint, used to counter spoofing to be sure that member
-    /// belongs to a particular mint
-    pub mint: Pubkey,
-    /// The pubkey of the `TokenGroup`
-    pub group: Pubkey,
-    /// The member number
-    pub member_number: u32,
-}
-impl TokenGroupMember {
-    pub const LEN: usize = 8 + 32 + 32 + 4;
-    /// Creates a new `TokenGroupMember` state
-    pub fn new(mint: &Pubkey, group: &Pubkey, member_number: u32) -> Self {
-        Self {
-            mint: *mint,
-            group: *group,
-            member_number,
-        }
-    }
-}
