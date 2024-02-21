f194d693c57f384373535d083df058d474cb77ce
Balmy Gazebo <balmy.gazebo_0f@icloud.com>
2024-02-21 15:18:30 -0500

    Changed Initialize Group Account

--- examples/src/utils/distribution.ts
+++ examples/src/utils/distribution.ts
@@ -1,11 +1,12 @@
 import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { getATAAddressSync, getDistributionAccount, getDistributionProgram } from "./core";
+import { getATAAddressSync, getDistributionAccount, getDistributionProgram, getGroupAccount } from "./core";
 import { Provider } from "@coral-xyz/anchor";
 import { TOKEN_PROGRAM_ID } from "./constants";
 
 export const buildAddDistributionIx = async (provider: Provider, collection: string, authority: string) => {
     const distributionProgram = getDistributionProgram(provider);
     const distributionAccount = getDistributionAccount(collection);
+    const groupAccount = getGroupAccount(collection);
     
     const authorityPubkey = new PublicKey(authority);
 
@@ -14,7 +15,7 @@ export const buildAddDistributionIx = async (provider: Provider, collection: str
         .accountsStrict({
             payer: authorityPubkey,
             authority: authorityPubkey,
-            mint: collection,
+            group: groupAccount,
             systemProgram: SystemProgram.programId,
             distribution: distributionAccount,
         })
--- programs/wen-royalty-distribution/src/state.rs
+++ programs/wen-royalty-distribution/src/state.rs
@@ -18,3 +18,17 @@ impl DistributionAccount {
 }
 
 pub const ROYALTY_BASIS_POINTS_FIELD: &str = "royalty_basis_points";
+
+/// Data struct for a `TokenGroup`
+#[account()]
+pub struct TokenGroup {
+    /// The authority that can sign to update the group
+    pub update_authority: Pubkey,
+    /// The associated mint, used to counter spoofing to be sure that group
+    /// belongs to a particular mint
+    pub mint: Pubkey,
+    /// The current number of group members
+    pub size: u32,
+    /// The maximum number of group members
+    pub max_size: u32,
+}
