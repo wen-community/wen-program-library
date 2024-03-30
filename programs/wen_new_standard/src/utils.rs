9d557b00e8cfa50c2471f08375175bd4dff36726
L0STE <125566964+L0STE@users.noreply.github.com>
2024-03-30 19:10:33 +0100

    Updated Utils to account for overflow

--- programs/wen_new_standard/src/utils.rs
+++ programs/wen_new_standard/src/utils.rs
@@ -32,13 +32,14 @@ pub fn update_account_lamports_to_minimum_balance<'info>(
     payer: AccountInfo<'info>,
     system_program: AccountInfo<'info>,
 ) -> Result<()> {
-    let extra_lamports = Rent::get()?.minimum_balance(account.data_len()) - account.get_lamports();
-    if extra_lamports > 0 {
+    if Rent::get()?.minimum_balance(account.data_len()) > account.get_lamports() {
+        let extra_lamports = Rent::get()?.minimum_balance(account.data_len()) - account.get_lamports();
         invoke(
             &transfer(payer.key, account.key, extra_lamports),
             &[payer, account, system_program],
         )?;
     }
+
     Ok(())
 }
 
